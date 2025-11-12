ea9bfe959be537ab5896163752c4531d6386cb02
Oziel Sousa <68363093+ozzysousa@users.noreply.github.com>
2025-11-12 14:59:19 -0300

    Refactor Tailwind config for clarity and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@
 module.exports = {
   // ESSENCIAL: Diz ao Tailwind onde procurar suas classes
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Garante que ele procure em todos os seus arquivos de componente
-    "./*.html",
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      // Define a paleta de cores para as classes customizadas se houver
+      // Cores personalizadas que você pode usar, como 'roxo-ressona'
       colors: {
-        'roxo-ressona': '#A855F7', // Exemplo: purple-500
-        'ciano-ressona': '#06B6D4', // Exemplo: cyan-500
+        'roxo-ressona': '#A855F7',
+        'ciano-ressona': '#06B6D4',
       }
     },
   },
